90a820b73a9c7ea2a6eb02f2b0658fdf9e6a3083
Peter Rzepniewski <peterr134@nycstudents.net>
2023-09-28 08:22:20 -0400

    Initial Commit

---
+++ src/CircleRunner.java
@@ -0,0 +1,17 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle cir1 = new Circle(5);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        cir1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //roughly 75, plus a little bit (maybe closer to 80)
+
+        // D. run the code and confirm your prediction! Were you correct?
+
+        //actual answer was 78.5, so basically what I predicted
+    }
+}
